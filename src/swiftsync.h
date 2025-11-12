0c9e2b2bc2c53517bfbcc87ebe7a49571e41df43
rustaceanrob <rob.netzke@gmail.com>
2025-11-12 10:05:25 +0000

    swiftsync: Add hintfile

--- src/swiftsync.h
+++ src/swiftsync.h
@@ -1,6 +1,9 @@
 #include <cstdint>
 #include <hash.h>
 #include <primitives/transaction.h>
+#include <streams.h>
+#include <uint256.h>
+#include <vector>
 
 namespace swiftsync {
 /** An aggregate for the SwiftSync protocol.
@@ -23,5 +26,43 @@ public:
     bool IsZero() const { return m_limb0 == 0 && m_limb1 == 0; }
     void Add(const COutPoint& outpoint);
     void Spend(const COutPoint& outpoint);
+};
+
+/**
+ * The unspent indexes of a block, assuming the list of block
+ * outputs has been flattened.
+ * */
+class BlockUnspentHints
+{
+private:
+    std::vector<uint64_t> m_unspent_indexes;
+
+public:
+    BlockUnspentHints(const std::vector<uint64_t> offsets);
+    bool IsUnspent(const uint64_t index);
+};
+
+/**
+ * A file that encodes the UTXO set state at a particular block hash.
+ */
+class Hintfile
+{
+private:
+    AutoFile m_file;
+    uint256 m_stop_hash;
+    uint32_t m_stop_height;
+    Hintfile(AutoFile& file);
+    Hintfile(AutoFile& file, const uint256& stop_hash, const uint32_t& stop_height);
+
+public:
+    // Construct a hint file and immediately read the stop hash and height.
+    static Hintfile FromExisting(AutoFile& file);
+    // Construct a hint file and write the stop hash and height.
+    static Hintfile CreateNew(AutoFile& file, const uint256& stop_hash, const uint32_t& stop_height);
+    BlockUnspentHints ReadNextBlock();
+    // Write the offsets of the unspent indexes to file, returning if the file commitment was successful.
+    bool WriteNextBlock(const std::vector<uint64_t>& unspent_offsets);
+    uint256 StopHash() { return m_stop_hash; };
+    uint32_t StopHeight() { return m_stop_height; };
 };
 }
